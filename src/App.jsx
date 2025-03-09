c6104c956439bc6856d6a383fc1e47756c086b74
Guilherme Vilas Boas Roberto <guilherme.vb.roberto@gmail.com>
2025-03-09 21:00:34 +0000

    Atualiza informações de contato e perfis sociais no App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Guilherme Roberto",
+  title: "Software engineer & Content Creator",
+  email: "guilherme.vb.roberto@gmail.com",
+  gitHub: "guintoki",
+  instagram: "guintoki.g",
+  linkedIn: "guilherme-vilas-boas-roberto",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "guintoki",
+  youTube: "guintoki",
 };
 
 const primaryColor = "#4E567E";
